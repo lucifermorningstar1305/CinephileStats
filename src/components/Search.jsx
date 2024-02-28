2609cea134064b3b73e6748f2f8ed87ccbd69560
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2024-02-29 12:40:55 +1300

    Fixed: useEffect dependency array

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,17 +3,20 @@ import { useEffect, useRef } from "react";
 const Search = ({ query, setQuery }) => {
   const inputElement = useRef(null);
 
-  useEffect(function () {
-    function callback(e) {
-      if (document.activeElement === inputElement.current) return;
-      if (e.code === "Enter") {
-        inputElement.current.focus();
-        setQuery("");
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
       }
-    }
-    document.addEventListener("keydown", callback);
-    return () => document.addEventListener("keydown", callback);
-  }, []);
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <div>
